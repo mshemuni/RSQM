bb2c7d06113418222049b36a15bde29f84fde2ec
Mohammad Niaei Shameoni <m.shemuni@myrafproject.org>
2019-04-22 16:26:44 +0300

    Some corrections

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ host = "localhost"
 user = "root"
 #Password
 passwd = ""
+#port Number. 3306 for MySQL
+port_num = 3306
 #Database name
 database = "dummy"
 #Single value table name
@@ -46,7 +48,7 @@ fields_single = ['sqm_id', 'start_jd', 'jd', 'end_jd', 'mag', 'sqm_mag',
                  'moon_hori']
 
 #Dummy variable. Will change with UPS condition
-is_sqm_up = True
+is_ups_up = True
 
 #Create a logger object
 logger = env.Logger(verb=ver, debugger=deb)
@@ -70,51 +72,55 @@ start_jd = atm.jd(dag.n_dusk_twilight_end())
 #Calculate Dawn Twilight for site
 end_jd = atm.jd(dag.n_dawn_twilight_start())
 
-#Check if jd is between Dusk and Dawn
-if not start_jd < now_jd < end_jd:
-    #Create an SQL object
-    sql = rmt.sql(host, user, passwd, database, port=3306,
-                  verb=ver, debugger=deb)
-    
-    #Calculate moon coordinates
-    the_moon = moon.coordinates(now_jd)
-    
-    #Create a data handler
-    sdh = data.handle(the_port, verb=ver, debugger=deb)
-    #Get average mesurments for 100 samples
-    data_mean = sdh.mean_read(number_of_samples_for_mean)
-    
-    #Organize average data for fields
-    #The data is: 'sqm_id', 'start_jd', 'jd', 'end_jd', 'mag', 'sqm_mag_stdv',
-    #           'sqm_mag', 'mag_stdv', 'temper', 'temper_stdv', 'moon_alt',
-    #           'moon_az', 'moon_phase', 'moon_mag', 'moon_hori',
-    #           'sample_number', 'valid_data'
-    sql_data_mean = [1, start_jd, data_mean[0], end_jd, data_mean[1],
-                     data_mean[2], data_mean[3], data_mean[4], data_mean[5],
-                     data_mean[6], the_moon[0],
-                     the_moon[1], the_moon[3], the_moon[4], the_moon[2],
-                     data_mean[7], data_mean[8]]
-    
-    #Insert data to corresponding table
-    sql.insert(mean_table, fields_mean, [sql_data_mean])
-    
-    #Read a single value
-    data_single = sdh.read()
-    
-    #Organize single data for fields
-    #The data is: 'sqm_id', 'start_jd', 'jd', 'end_jd', 'mag', 'sqm_mag',
-    #             'temper', 'moon_alt', 'moon_az', 'moon_phase', 'moon_mag',
-    #             'moon_hori'
-    sql_data_single = [1, start_jd, data_single[0], end_jd, data_single[1],
-                       data_single[2], data_single[3],
-                       the_moon[0], the_moon[1], the_moon[3], the_moon[4],
-                       the_moon[2]]
-    #Insert data to corresponding table
-    sql.insert(single_table, fields_single, [sql_data_single])
-    #Close sql connection
-    sql.close()
-    
-    
+#Check if UPS is healthy
+if is_ups_up:
+    #Check if jd is between Dusk and Dawn
+    if start_jd < now_jd < end_jd:
+        #Create an SQL object
+        sql = rmt.sql(host, user, passwd, database, port=port_num,
+                      verb=ver, debugger=deb)
+        
+        #Calculate moon coordinates
+        the_moon = moon.coordinates(now_jd)
+        
+        #Create a data handler
+        sdh = data.handle(the_port, verb=ver, debugger=deb)
+        #Get average mesurments for number_of_samples_for_mean samples
+        data_mean = sdh.mean_read(number_of_samples_for_mean)
+        
+        #Organize average data for fields
+        #The data is: 'sqm_id', 'start_jd', 'jd', 'end_jd', 'mag', 'sqm_mag_stdv',
+        #           'sqm_mag', 'mag_stdv', 'temper', 'temper_stdv', 'moon_alt',
+        #           'moon_az', 'moon_phase', 'moon_mag', 'moon_hori',
+        #           'sample_number', 'valid_data'
+        sql_data_mean = [1, start_jd, data_mean[0], end_jd, data_mean[1],
+                         data_mean[2], data_mean[3], data_mean[4], data_mean[5],
+                         data_mean[6], the_moon[0],
+                         the_moon[1], the_moon[3], the_moon[4], the_moon[2],
+                         data_mean[7], data_mean[8]]
+        
+        #Insert data to corresponding table
+        sql.insert(mean_table, fields_mean, [sql_data_mean])
+        
+        #Read a single value
+        data_single = sdh.read()
+        
+        #Organize single data for fields
+        #The data is: 'sqm_id', 'start_jd', 'jd', 'end_jd', 'mag', 'sqm_mag',
+        #             'temper', 'moon_alt', 'moon_az', 'moon_phase', 'moon_mag',
+        #             'moon_hori'
+        sql_data_single = [1, start_jd, data_single[0], end_jd, data_single[1],
+                           data_single[2], data_single[3],
+                           the_moon[0], the_moon[1], the_moon[3], the_moon[4],
+                           the_moon[2]]
+        #Insert data to corresponding table
+        sql.insert(single_table, fields_single, [sql_data_single])
+        #Close sql connection
+        sql.close()
+        
+    else:
+        #Log a line about no observation at given time
+        logger.log("No observation at {}.".format(now_jd))
 else:
-    #Log a line about no observation at given time
-    logger.log("No observation at {}.".format(now_jd))
+    #Log a line about bad UPS condition
+    logger.log("Bad UPS condition {}".format(now_jd))
